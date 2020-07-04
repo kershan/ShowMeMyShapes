91aa01b99a59f0140b8715332e5494238bdbf6e2
Kershan Naidoo <n.kershan@gmail.com>
2020-07-04 12:04:05 +0100

    Initial clear screen for GLES

---
+++ app/src/main/java/online/platformer/showmemyshapes/MainActivity.kt
@@ -0,0 +1,33 @@
+package online.platformer.showmemyshapes
+
+import android.app.Activity
+import android.content.Context
+import android.opengl.GLSurfaceView
+import android.os.Bundle
+
+class MainActivity : Activity() {
+
+    private lateinit var glView: GLSurfaceView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        glView = MyGLSurfaceView(this)
+
+        setContentView(glView)
+    }
+
+    inner class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
+        private val renderer: MyGLRenderer
+
+        init {
+            setEGLContextClientVersion(2)
+
+            renderer = MyGLRenderer()
+
+            setRenderer(renderer)
+
+            renderMode = RENDERMODE_WHEN_DIRTY
+        }
+    }
+}
