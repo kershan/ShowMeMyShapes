af2adf0d5defd2487e2283ed046c9c3104e38fa2
Kershan Naidoo <n.kershan@gmail.com>
2020-07-04 15:23:32 +0100

    Add user input for rotation.

--- app/src/main/java/online/platformer/showmemyshapes/MainActivity.kt
+++ app/src/main/java/online/platformer/showmemyshapes/MainActivity.kt
@@ -4,9 +4,11 @@ import android.app.Activity
 import android.content.Context
 import android.opengl.GLSurfaceView
 import android.os.Bundle
+import android.view.MotionEvent
+
+private const val TOUCH_SCALE_FACTOR: Float = 180.0f / 320f
 
 class MainActivity : Activity() {
-
     private lateinit var glView: GLSurfaceView
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -20,6 +22,9 @@ class MainActivity : Activity() {
     inner class MyGLSurfaceView(context: Context) : GLSurfaceView(context) {
         private val renderer: MyGLRenderer
 
+        private var previousX = 0f
+        private var previousY = 0f
+
         init {
             setEGLContextClientVersion(2)
 
@@ -27,7 +32,30 @@ class MainActivity : Activity() {
 
             setRenderer(renderer)
 
-            //renderMode = RENDERMODE_WHEN_DIRTY
+            renderMode = RENDERMODE_WHEN_DIRTY
+        }
+
+        override fun onTouchEvent(event: MotionEvent?): Boolean {
+            val x = event?.x ?: 0f
+            val y = event?.y ?: 0f
+
+            when (event?.action) {
+                MotionEvent.ACTION_MOVE -> {
+                    var dx = x - previousX
+                    var dy = x - previousY
+
+                    if (y > height / 2) dx *= -1
+
+                    if (x > width / 2) dy *= -1
+
+                    renderer.angle = (dx + dy) * TOUCH_SCALE_FACTOR
+                    requestRender()
+                }
+            }
+
+            previousX = x
+            previousY = y
+            return true
         }
     }
 }
--- app/src/main/java/online/platformer/showmemyshapes/MyGLRenderer.kt
+++ app/src/main/java/online/platformer/showmemyshapes/MyGLRenderer.kt
@@ -3,7 +3,6 @@ package online.platformer.showmemyshapes
 import android.opengl.GLES20
 import android.opengl.GLSurfaceView
 import android.opengl.Matrix
-import android.os.SystemClock
 import javax.microedition.khronos.egl.EGLConfig
 import javax.microedition.khronos.opengles.GL10
 
@@ -15,6 +14,9 @@ class MyGLRenderer : GLSurfaceView.Renderer {
     private val viewMatrix = FloatArray(16)
     private val modelViewProjectionMatrix = FloatArray(16)
     private val rotationMatrix = FloatArray(16)
+
+    @Volatile
+    var angle = 0f
 
     override fun onSurfaceCreated(p0: GL10?, config: EGLConfig?) {
         GLES20.glClearColor(0f, 0f, 0f, 1f)
@@ -42,8 +44,8 @@ class MyGLRenderer : GLSurfaceView.Renderer {
         Matrix.multiplyMM(modelViewProjectionMatrix, 0, projectionMatrix, 0, viewMatrix, 0)
 
         val scratch = FloatArray(16)
-        val time = SystemClock.uptimeMillis() % 4000
-        val angle = 0.090f * time
+        /*val time = SystemClock.uptimeMillis() % 4000
+        val angle = 0.090f * time*/
         Matrix.setRotateM(rotationMatrix, 0, angle, 0f, 0f, -1f)
 
         Matrix.multiplyMM(scratch, 0, modelViewProjectionMatrix, 0, rotationMatrix, 0)
